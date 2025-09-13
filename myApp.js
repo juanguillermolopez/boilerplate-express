b0319b0ab5999da57d89ae4ef6635844b27cf560
Guillermo López <juan.guillermo.lopez.garcia@gmail.com>
2025-09-13 16:00:36 -0300

    funcion hello express

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let app = express();
 // Agregar un mensaje en consola
 console.log("Hello World");
 
+
+app.get('/', function(req, res) {
+  res.send('Hello Express');
+});
+
 module.exports = app;
 
 
